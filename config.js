d44e52a4cd43e1e520eef8dcd0240e43f0b91311
vuhieu441 <vuhieu441@gmail.com>
2021-09-06 20:26:52 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle: 'Trang à!',
+    introDesc: `Trái đất vốn lạ thường 🥰
+    Mà sao em cứ đi nhầm đường 😙
+    Lạc vào tim anh lẻ loi.
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'Chọn vào đây đi ',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Thích ',
+    btnNo: 'Không thích',
+    question:'Trên thế giới hơn 7 tỉ người mà sao Trang lại yêu mình <3',
+    btnReply: 'Gửi cho tin nhắn cho mình nha <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Mình biết mà 🥰. Yêu Trang nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 9h gặp nhau trong PUPG nhé công chúa.',
+    btnAccept: 'Oke nà <3',
+    messLink: 'http://fb.com' 
+}
